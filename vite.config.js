b0c7c46478ec25b9e242413559eb237bf0214ffe
Vaishnavan S <vaishnavans31@gmail.com>
2026-01-01 20:59:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`, // Sometimes helpful for older components
-  },
   base: './',
 })
